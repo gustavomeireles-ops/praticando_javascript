d95d86d664c23773aa279efcef88ba5b75996a2c
GUSTAVO CASTILHO DE MEIRELES <57034439893@info.local>
2026-09-15 17:30:09 -0300

    :sparkles: feat: Sript.js finalizado

--- repeticao.js
+++ repeticao.js
@@ -11,5 +11,18 @@ for (let i = 1; i <= 10; i++) {
 const numero_taboada = 2;
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`${numero_taboada} * ${i} = ${numero_taboada * i}`);
+  console.log(`${numero_taboada} x ${i} = ${numero_taboada * i}`);
 }
+
+//* 3° Soma dos primeiros N números naturais
+
+const numeroN = 6;
+let soma = 0;
+
+for (let i = 1; i <= N; i++) {
+  soma += i;
+}
+
+console.log(`O resultado é da soma dos primeiros números até N dá: ${soma}`);
+
+//* 4° 
